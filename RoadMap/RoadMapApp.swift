f5112899e2a193076a1df177938c297e303b80ec
ad <ad>
2024-05-08 18:03:14 +0500

    Initial Commit

---
+++ RoadMap/RoadMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadMapApp.swift
+//  RoadMap
+//
+//  Created by Adlet Zhantassov on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoadMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
